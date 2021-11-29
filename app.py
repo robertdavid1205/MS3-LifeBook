febd7cc5f6a0d77f3ab2bbca85ad9c339b9b2fb8
robertdavid1205 <robertdavid1205@gmail.com>
2021-11-29 17:11:59 +0000

    create add_book template

--- app.py
+++ app.py
@@ -102,6 +102,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_book")
+def add_book():
+    return render_template("add_book.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
