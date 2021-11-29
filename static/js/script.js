269b85fcb7989d685b68e4793e1ccfcbd04abd04
robertdavid1205 <robertdavid1205@gmail.com>
2021-11-29 17:54:13 +0000

    add book form

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $(".sidenav").sidenav();
     $('.collapsible').collapsible();
     $('.materialboxed').materialbox();
+    $('.datepicker').datepicker({
+      format: "dd mmmm, yyyy",
+      yearRange: 100,
+      showClearBtn: true,
+      i18n:{
+        done: "Select"
+      }
+    });
   });
